976535e0030bfc9537e9e70fbeda3745c4e20c2b
Aayush Dhiman <aayushdhiman@gmail.com>
2019-03-11 09:29:42 -0400

    Update BalancingParenthesis.java
    Added Javadoc to completed project.

--- BalancingParenthesis/BalancingParenthesis.java
+++ BalancingParenthesis/BalancingParenthesis.java
@@ -7,7 +7,91 @@ public class BalancingParenthesis {
 	private static Stack stack = new Stack();
 	
 	public static void balanceEquation(String[] e) {
-		for (int i = 0; i < e.length; i++) {
+		for (int i = 0; i < e.length; i++) {package BalancingParenthesis;
+
+import java.util.*;
+
+public class BalancingParenthesis {
+
+    private static Stack stack = new Stack();
+
+    /**
+     * Checks if the equation is balanced by pushing and popping parenthesis off of the stack.
+     * @param e the equation to check for balance
+     */
+    public static void balanceEquation(String[] e) {
+        for (int i = 0; i < e.length; i++) {
+            if (e[i].equals("(")) {
+                stack.push("(");
+            } else if (e[i].equals("[")) {
+                stack.push("[");
+            } else if (e[i].equals("{")) {
+                stack.push("{");
+            } else if (e[i].equals(")")) {
+                if (!stack.empty()) {
+                    if (stack.peek().equals("(")) {
+                        stack.pop();
+                    } else {
+                        System.out.println("Formula is not balanced.\n");
+                        return;
+                    }
+                } else {
+                    System.out.println("Formula is not balanced.\n");
+                    return;
+                }
+            } else if (e[i].equals("]")) {
+                if (!stack.empty()) {
+                    if (stack.peek().equals("[")) {
+                        stack.pop();
+                    } else {
+                        System.out.println("Formula is not balanced.\n");
+                        return;
+                    }
+                } else {
+                    System.out.println("Formula is not balanced.\n");
+                    return;
+                }
+            } else if (e[i].equals("}")) {
+                if (!stack.empty()) {
+                    if (stack.peek().equals("{")) {
+                        stack.pop();
+                    } else {
+                        System.out.println("Formula is not balanced.\n");
+                        return;
+                    }
+                } else {
+                    System.out.println("Formula is not balanced.\n");
+                    return;
+                }
+            }
+        }
+        if (stack.empty()) {
+            System.out.println("Formula is balanced.\n");
+        } else {
+            System.out.println("Formula is not balanced.\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter your equation: ");
+            String equation = sc.nextLine();
+
+            if (equation.equalsIgnoreCase("")) {
+                System.out.println("Empty input, program complete.");
+                return;
+            } else {
+                String[] eq = equation.split("");
+                balanceEquation(eq);
+            }
+
+        }
+
+    }
+}
+
 			if (e[i].equals("(")) {
 				stack.push("(");
 			} else if (e[i].equals("[")) {
